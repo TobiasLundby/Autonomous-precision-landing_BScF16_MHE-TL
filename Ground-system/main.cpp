35dc87ccc30ac9ed3c8e540ac8c768cf162bb272
TobiasLundby <tobiaslundby@gmail.com>
2016-03-09 10:31:42 +0100

    Added CMake file and main.cpp which includes the proper OpenCV libraries

---
+++ Ground-system/main.cpp
@@ -0,0 +1,39 @@
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void drawText(Mat & image);
+
+int main()
+{
+    cout << "Built with OpenCV " << CV_VERSION << endl;
+    Mat image;
+    VideoCapture capture;
+    capture.open(0);
+    if(capture.isOpened())
+    {
+        cout << "Capture is opened" << endl;
+        for(;;)
+        {
+            capture >> image;
+            if(image.empty())
+                break;
+            imshow("Sample", image);
+            if(waitKey(10) >= 0)
+                break;
+        }
+    }
+    else
+    {
+        cout << "No capture" << endl;
+        image = Mat::zeros(480, 640, CV_8UC1);
+        imshow("Sample", image);
+        waitKey(0);
+    }
+    return 0;
+}
