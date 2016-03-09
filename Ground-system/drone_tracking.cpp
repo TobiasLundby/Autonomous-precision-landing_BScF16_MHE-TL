0be88e2d6f897a2a869977d5c8f732d7b8e56504
mhoejgaard <mhoejgaard@hotmail.com>
2016-03-09 13:10:26 +0100

    Added class structure

---
+++ Ground-system/drone_tracking.cpp
@@ -0,0 +1,26 @@
+/*****************************************************************************
+* University of Southern Denmark
+* UAS Center
+* Mathias Højgaard Egeberg & Tobias Lundby
+*
+* MODULENAME.: drone_tracking.cpp
+* PROJECT....: Autonomous precision landing ground system
+* DESCRIPTION: See module specification file (.h-file).
+*
+*****************************************************************************/
+
+
+// Function dummy
+/*****************************************************************************
+*   Function : See module specification (.h-file).
+*****************************************************************************/
+
+
+
+
+
+
+
+
+
+/****************************** End Of Module *******************************/
--- Ground-system/main.cpp
+++ Ground-system/main.cpp
@@ -1,8 +1,21 @@
+/*****************************************************************************
+* University of Southern Denmark
+* UAS Center
+* Mathias Højgaard Egeberg & Tobias Lundby
+*
+* MODULENAME.: main.cpp
+* PROJECT....: Autonomous precision landing ground system
+* DESCRIPTION: ???
+*
+*****************************************************************************/
+
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/videoio.hpp"
 #include <iostream>
+
+#include "drone_tracking.hpp"
 
 using namespace cv;
 using namespace std;
@@ -11,15 +24,28 @@ void drawText(Mat & image);
 
 int main(int argc,char** argv)
 {
-    cout << "argc: "<<argc << endl;
-
     cout << "Built with OpenCV " << CV_VERSION << endl;
     Mat image;
     VideoCapture capture;
-    capture.open(0);
+
+// Open specified video file or webcam
+    if(argc==2) // If two arguments, open file
+    {
+      cout << "Opening video"<< endl;
+      capture.open(argv[2]);            // Open video file
+
+    }
+    else
+    {
+      cout << "Opening webcam" << endl;
+      capture.open(0);                  // Open webcam
+    }
+
+// Test if capture is opened
     if(capture.isOpened())
     {
         cout << "Capture is opened" << endl;
+// Processing
         for(;;)
         {
             capture >> image;
@@ -30,7 +56,7 @@ int main(int argc,char** argv)
                 break;
         }
     }
-    else
+    else    // Error capture is not opened
     {
         cout << "No capture" << endl;
         image = Mat::zeros(480, 640, CV_8UC1);
