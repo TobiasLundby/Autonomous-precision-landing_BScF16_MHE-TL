a991e00b31e83c09ea53572d9c3f6a6279c567c3
TobiasLundby <tobiaslundby@gmail.com>
2016-03-15 15:13:16 +0100

    A bit cleaned code. Working but not as it should

--- Ground-system/drone_tracking.hpp
+++ Ground-system/drone_tracking.hpp
@@ -36,6 +36,7 @@ private: // Methods
   void show_frame(string, Mat);
   void frame_analysis();
   void diode_detection();
+  int midpoint_circle_algorithm(Mat&, int, int, int);
 
 private: // Variables
   string filename;
@@ -56,6 +57,8 @@ private: // Variables
   int gaussian_blur         = 11; //Must be positive and odd
   Mat frame_hsv;
   Mat frame_red;
+  Mat frame_red_hsv;
+  Mat frame_red_split[3];
   Mat frame_gray;
   Mat frame_gray_with_Gblur;
   Mat mask_red;
@@ -130,7 +133,7 @@ void drone_tracking::frame_analysis()
 {
   // ALL THE ANALYSIS METHODS SHOULD BE CALLED HERE - THIS IS THE MASTER
   // ALL THE ANALYSIS METHODS SHOULD BE CALLED HERE - THIS IS THE MASTER
-  show_frame(video_window_text, frame_bgr);
+  //show_frame(video_window_text, frame_bgr);
   diode_detection();
 }
 
@@ -143,16 +146,10 @@ void drone_tracking::diode_detection()
 {
   cvtColor(frame_bgr, frame_hsv, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
   cvtColor(frame_bgr, frame_gray, COLOR_BGR2GRAY); //Convert the captured frame from BGR to HSV
-  show_frame("Gray frame", frame_gray);
   GaussianBlur(frame_gray, frame_gray_with_Gblur, Size(gaussian_blur, gaussian_blur), 0);
-
-  //minMaxLoc(frame_gray_with_Gblur, &m, &M, &p_min, &p_max);
-  //cout << "min: " << m << " at " << p_min << " max: " << M << " at " << p_max << endl;
-  //circle(frame_gray_with_Gblur, p_max, 40, Scalar(255, 0, 0), 5);
 
   // Setup SimpleBlobDetector parameters.
 	SimpleBlobDetector::Params params;
-
   // Change thresholds
 	//params.minThreshold = 0;
 	//params.maxThreshold = 100;
@@ -163,7 +160,7 @@ void drone_tracking::diode_detection()
   params.maxArea = 100;
 	// Filter by Circularity
 	params.filterByCircularity = true;
-	params.minCircularity = 0.1;
+	params.minCircularity = 0.5;
 	// Filter by Convexity
 	//params.filterByConvexity = true;
 	//params.minConvexity = 0.87;
@@ -176,26 +173,89 @@ void drone_tracking::diode_detection()
   // Set up detector with params
 	Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
 	// Detect blobs
-	detector->detect( frame_gray_with_Gblur, keypoints);
+	detector->detect(frame_gray_with_Gblur, keypoints);
   Mat im_with_keypoints;
 	drawKeypoints( frame_gray_with_Gblur, keypoints, im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
   //cout << "There are " << keypoints.size() << " keypoints" << endl;
 
+  inRange(frame_hsv, Scalar(hsv_h_red_low,hsv_s_red_low,hsv_v_red_low), Scalar(hsv_h_red_upper, hsv_s_red_upper, hsv_v_red_upper), mask_red);
+  frame_red = Scalar(0);
+  frame_bgr.copyTo(frame_red, mask_red);
+  split(frame_red,frame_red_split);
+
+  cvtColor(frame_red, frame_red_hsv, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
   if (keypoints.size()) {
-    cout << "First has size " << keypoints[0].size << endl;
-    circle(im_with_keypoints, keypoints[0].pt, keypoints[0].size * 5, Scalar(255, 0, 0), 5);
+    //cout << "value " << midpoint_circle_algorithm(frame_hsv, keypoints[0].pt.x, keypoints[0].pt.y, keypoints[0].size) << endl;
+  }
+
+  int total_avg_hue_value;
+  if (keypoints.size()) {
+    for (size_t i = 0; i < keypoints.size(); i++) {
+      int iterations = 0;
+      int avg_hue_value = 0;
+      for (int j = keypoints[i].size; j < keypoints[i].size * 1.5; j++) {
+        iterations++;
+        avg_hue_value += midpoint_circle_algorithm(mask_red, keypoints[i].pt.x, keypoints[i].pt.y, j);
+      }
+      total_avg_hue_value = avg_hue_value / iterations;
+      if (total_avg_hue_value > 15) {
+        cout << "Fucking red diode" << endl;
+        circle(im_with_keypoints, keypoints[i].pt, keypoints[i].size, Scalar(255-(i*10), 0, 0), keypoints[i].size *1.5);
+      }
+      //cout << "Index " << i << " has avg value " << total_avg_hue_value << endl;
+      //circle(im_with_keypoints, keypoints[i].pt, keypoints[i].size, Scalar(255-(i*10), 0, 0), keypoints[i].size);
+    }
   }
 
   // Show blobs
 	imshow("keypoints", im_with_keypoints );
 
-  //show_frame("Gray frame with blur", frame_gray_with_Gblur);
+  //show_frame("Channel frame", frame_red_split[0]);
+  show_frame("Red frame", mask_red);
+}
 
-  inRange(frame_hsv, Scalar(hsv_h_red_low,hsv_s_red_low,hsv_v_red_low), Scalar(hsv_h_red_upper, hsv_s_red_upper, hsv_v_red_upper), mask_red);
-  frame_red = Scalar(0);
-  frame_bgr.copyTo(frame_red, mask_red);
-  show_frame("Red frame", frame_red);
+int drone_tracking::midpoint_circle_algorithm(Mat& image, int x0, int y0, int radius)
+/*****************************************************************************
+*   Input    : x
+*   Output   :
+*   Function :
+*   Ref      : https://en.wikipedia.org/wiki/Midpoint_circle_algorithm
+******************************************************************************/
+{
+  int x = radius;
+  int y = 0;
+  int decisionOver2 = 1 - x;   // Decision criterion divided by 2 evaluated at x=r, y=0
+  int sum = 0;
+  int pixel_count = 0;
+
+  while( y <= x )
+  {
+    pixel_count += 8;
+    sum += image.at<uchar>(x + x0, y + y0); // Octant 1
+    sum += image.at<uchar>(y + x0, x + y0); // Octant 2
+    sum += image.at<uchar>(-x + x0, y + y0); // Octant 4
+    sum += image.at<uchar>(-y + x0, x + y0); // Octant 3
+    sum += image.at<uchar>(-y + x0, -x + y0); // Octant 6
+    sum += image.at<uchar>(-x + x0, -y + y0); // Octant 5
+    sum += image.at<uchar>(x + x0, -y + y0); // Octant 7
+    sum += image.at<uchar>(y + x0, -x + y0); // Octant 8
+
+    y++;
+    if (decisionOver2<=0)
+    {
+      decisionOver2 += 2 * y + 1;   // Change in decision criterion for y -> y+1
+    }
+    else
+    {
+      x--;
+      decisionOver2 += 2 * (y - x) + 1;   // Change for y -> y+1, x -> x-1
+    }
+  }
+  if (pixel_count)
+    return sum / pixel_count;
+  return 1;
 }
 
 
