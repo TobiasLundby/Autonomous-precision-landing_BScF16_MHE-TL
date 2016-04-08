7193d128e0de7d1aaf83c70a384a369154b50f26
Tobias Lundby <tobiaslundby@gmail.com>
2016-04-08 13:59:44 +0200

    Made changes

--- Drone-system/wiringPi_test/DSM_analyser.cpp
+++ Drone-system/wiringPi_test/DSM_analyser.cpp
@@ -155,7 +155,7 @@ int main(int argc,char* argv[])
                     change_packet_values(package_in, package_out);
                     packet_modified = true; // Do it once per packet
                     printf("Change\n");
-                    printf("Channel 0 value %X%X", package_out.byte_H[0+1], package_out.byte_H[0+1] )
+                    printf("Channel 0 value %X%X", package_out.byte_H[0+1], package_out.byte_H[0+1] );
                 } else if (!modify_packets)
                     package_out = package_in;
 
