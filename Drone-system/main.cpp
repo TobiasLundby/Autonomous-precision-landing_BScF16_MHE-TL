5a9b25fbd23368c01ee6c6b17dde6653632831a3
MHoejgaard <mhoejgaard@hotmail.com>
2016-05-19 19:33:49 +0200

    Commmit before getting working code from drone

--- Drone-system/main.cpp
+++ Drone-system/main.cpp
@@ -89,51 +89,51 @@ int main(int argc,char* argv[])
     bool error = false;
 
     /* Various for client start */
-      socket_client socket;
-      socket_package sock_pack_in, sock_pack_out;
-      sock_pack_out.field0=0;
-      sock_pack_out.field1=1;
-      sock_pack_out.field2=2;
-      sock_pack_out.field3=3;
-      sock_pack_out.field4=4;
-      sock_pack_out.field5=5;
-      sock_pack_out.field6=6;
-      sock_pack_out.field7=7;
-      sock_pack_out.field8=0;
-      sock_pack_out.field9=0;
-      sock_pack_out.field10=0;
-      sock_pack_out.field11=0;
-      sock_pack_out.field12=0;
-      sock_pack_out.field13=0;
-      sock_pack_out.field14=0;
-      sock_pack_out.field15=0;
-      sock_pack_out.field16=0;
-      sock_pack_out.field17=0;
-      sock_pack_out.field18=0;
-      sock_pack_out.field19=1;
-
-      socket.socket_send_frame(sock_pack_out);
-      socket.socket_get_frame(&sock_pack_in);
-      cout << sock_pack_in.field0;
-      cout << sock_pack_in.field1;
-      cout << sock_pack_in.field2;
-      cout << sock_pack_in.field3;
-      cout << sock_pack_in.field4;
-      cout << sock_pack_in.field5;
-      cout << sock_pack_in.field6;
-      cout << sock_pack_in.field7;
-      cout << sock_pack_in.field8;
-      cout << sock_pack_in.field9;
-      cout << sock_pack_in.field10;
-      cout << sock_pack_in.field11;
-      cout << sock_pack_in.field12;
-      cout << sock_pack_in.field13;
-      cout << sock_pack_in.field14;
-      cout << sock_pack_in.field15;
-      cout << sock_pack_in.field16;
-      cout << sock_pack_in.field17;
-      cout << sock_pack_in.field18;
-      cout << sock_pack_in.field19 << endl;
+      // socket_client socket;
+      // socket_package sock_pack_in, sock_pack_out;
+      // sock_pack_out.field0=0;
+      // sock_pack_out.field1=1;
+      // sock_pack_out.field2=2;
+      // sock_pack_out.field3=3;
+      // sock_pack_out.field4=4;
+      // sock_pack_out.field5=5;
+      // sock_pack_out.field6=6;
+      // sock_pack_out.field7=7;
+      // sock_pack_out.field8=0;
+      // sock_pack_out.field9=0;
+      // sock_pack_out.field10=0;
+      // sock_pack_out.field11=0;
+      // sock_pack_out.field12=0;
+      // sock_pack_out.field13=0;
+      // sock_pack_out.field14=0;
+      // sock_pack_out.field15=0;
+      // sock_pack_out.field16=0;
+      // sock_pack_out.field17=0;
+      // sock_pack_out.field18=0;
+      // sock_pack_out.field19=1;
+      //
+      // socket.socket_send_frame(sock_pack_out);
+      // socket.socket_get_frame(&sock_pack_in);
+      // cout << sock_pack_in.field0;
+      // cout << sock_pack_in.field1;
+      // cout << sock_pack_in.field2;
+      // cout << sock_pack_in.field3;
+      // cout << sock_pack_in.field4;
+      // cout << sock_pack_in.field5;
+      // cout << sock_pack_in.field6;
+      // cout << sock_pack_in.field7;
+      // cout << sock_pack_in.field8;
+      // cout << sock_pack_in.field9;
+      // cout << sock_pack_in.field10;
+      // cout << sock_pack_in.field11;
+      // cout << sock_pack_in.field12;
+      // cout << sock_pack_in.field13;
+      // cout << sock_pack_in.field14;
+      // cout << sock_pack_in.field15;
+      // cout << sock_pack_in.field16;
+      // cout << sock_pack_in.field17;
+      // cout << sock_pack_in.field18;
+      // cout << sock_pack_in.field19 << endl;
 
     /* Various for client end */
 
@@ -174,50 +174,50 @@ int main(int argc,char* argv[])
                 MAIN_STATE = MAIN_S_FORCE_DOWN;
                 break;
             case MAIN_S_FORCE_DOWN:
-
-                if (sock_pack_in.field0 > -OFFSET_MAX_RANGE and sock_pack_in.field0 < OFFSET_MAX_RANGE)
-                    ch0_off_goal = sock_pack_in.field0;
-                if (sock_pack_in.field1 > -OFFSET_MAX_RANGE and sock_pack_in.field1 < OFFSET_MAX_RANGE)
-                    ch1_off_goal = sock_pack_in.field1;
-                if (sock_pack_in.field2 > -OFFSET_MAX_RANGE and sock_pack_in.field2 < OFFSET_MAX_RANGE)
-                    ch2_off_goal = sock_pack_in.field2;
-                if (sock_pack_in.field3 > -OFFSET_MAX_RANGE and sock_pack_in.field3 < OFFSET_MAX_RANGE)
-                    ch3_off_goal = sock_pack_in.field3;
-                if (sock_pack_in.field4 > -OFFSET_MAX_RANGE and sock_pack_in.field4 < OFFSET_MAX_RANGE)
-                    ch4_off_goal = sock_pack_in.field4;
-                if (sock_pack_in.field5 > -OFFSET_MAX_RANGE and sock_pack_in.field5 < OFFSET_MAX_RANGE)
-                    ch5_off_goal = sock_pack_in.field5;
-                if (sock_pack_in.field6 > -OFFSET_MAX_RANGE and sock_pack_in.field6 < OFFSET_MAX_RANGE)
-                    ch6_off_goal = sock_pack_in.field6;
-
-                if (ch0_off > ch0_off_goal)
-                    ch0_off -= OFFSET_MAX_CHANGE;
-                else if (ch0_off < ch0_off_goal)
-                    ch0_off += OFFSET_MAX_CHANGE;
-                if (ch1_off > ch1_off_goal)
-                    ch1_off -= OFFSET_MAX_CHANGE;
-                else if (ch1_off < ch1_off_goal)
-                    ch1_off += OFFSET_MAX_CHANGE;
-                if (ch2_off > ch2_off_goal)
-                    ch2_off -= OFFSET_MAX_CHANGE;
-                else if (ch2_off < ch2_off_goal)
-                    ch2_off += OFFSET_MAX_CHANGE;
-                if (ch3_off > ch3_off_goal)
-                    ch3_off -= OFFSET_MAX_CHANGE;
-                else if (ch3_off < ch3_off_goal)
-                    ch3_off += OFFSET_MAX_CHANGE;
-                if (ch4_off > ch4_off_goal)
-                    ch4_off -= OFFSET_MAX_CHANGE;
-                else if (ch4_off < ch4_off_goal)
-                    ch4_off += OFFSET_MAX_CHANGE;
-                if (ch5_off > ch5_off_goal)
-                    ch5_off -= OFFSET_MAX_CHANGE;
-                else if (ch5_off < ch5_off_goal)
-                    ch5_off += OFFSET_MAX_CHANGE;
-                if (ch6_off > ch6_off_goal)
-                    ch6_off -= OFFSET_MAX_CHANGE;
-                else if (ch6_off < ch6_off_goal)
-                    ch6_off += OFFSET_MAX_CHANGE;
+                //
+                // if (sock_pack_in.field0 > -OFFSET_MAX_RANGE and sock_pack_in.field0 < OFFSET_MAX_RANGE)
+                //     ch0_off_goal = sock_pack_in.field0;
+                // if (sock_pack_in.field1 > -OFFSET_MAX_RANGE and sock_pack_in.field1 < OFFSET_MAX_RANGE)
+                //     ch1_off_goal = sock_pack_in.field1;
+                // if (sock_pack_in.field2 > -OFFSET_MAX_RANGE and sock_pack_in.field2 < OFFSET_MAX_RANGE)
+                //     ch2_off_goal = sock_pack_in.field2;
+                // if (sock_pack_in.field3 > -OFFSET_MAX_RANGE and sock_pack_in.field3 < OFFSET_MAX_RANGE)
+                //     ch3_off_goal = sock_pack_in.field3;
+                // if (sock_pack_in.field4 > -OFFSET_MAX_RANGE and sock_pack_in.field4 < OFFSET_MAX_RANGE)
+                //     ch4_off_goal = sock_pack_in.field4;
+                // if (sock_pack_in.field5 > -OFFSET_MAX_RANGE and sock_pack_in.field5 < OFFSET_MAX_RANGE)
+                //     ch5_off_goal = sock_pack_in.field5;
+                // if (sock_pack_in.field6 > -OFFSET_MAX_RANGE and sock_pack_in.field6 < OFFSET_MAX_RANGE)
+                //     ch6_off_goal = sock_pack_in.field6;
+                //
+                // if (ch0_off > ch0_off_goal)
+                //     ch0_off -= OFFSET_MAX_CHANGE;
+                // else if (ch0_off < ch0_off_goal)
+                //     ch0_off += OFFSET_MAX_CHANGE;
+                // if (ch1_off > ch1_off_goal)
+                //     ch1_off -= OFFSET_MAX_CHANGE;
+                // else if (ch1_off < ch1_off_goal)
+                //     ch1_off += OFFSET_MAX_CHANGE;
+                // if (ch2_off > ch2_off_goal)
+                //     ch2_off -= OFFSET_MAX_CHANGE;
+                // else if (ch2_off < ch2_off_goal)
+                //     ch2_off += OFFSET_MAX_CHANGE;
+                // if (ch3_off > ch3_off_goal)
+                //     ch3_off -= OFFSET_MAX_CHANGE;
+                // else if (ch3_off < ch3_off_goal)
+                //     ch3_off += OFFSET_MAX_CHANGE;
+                // if (ch4_off > ch4_off_goal)
+                //     ch4_off -= OFFSET_MAX_CHANGE;
+                // else if (ch4_off < ch4_off_goal)
+                //     ch4_off += OFFSET_MAX_CHANGE;
+                // if (ch5_off > ch5_off_goal)
+                //     ch5_off -= OFFSET_MAX_CHANGE;
+                // else if (ch5_off < ch5_off_goal)
+                //     ch5_off += OFFSET_MAX_CHANGE;
+                // if (ch6_off > ch6_off_goal)
+                //     ch6_off -= OFFSET_MAX_CHANGE;
+                // else if (ch6_off < ch6_off_goal)
+                //     ch6_off += OFFSET_MAX_CHANGE;
 
                 serial_con.change_channel_offsets(ch0_off,ch1_off,ch2_off,ch3_off,ch4_off,ch5_off,ch6_off);
 
@@ -226,19 +226,19 @@ int main(int argc,char* argv[])
                 //print_out_frame(serial_con);
 
                 if (serial_con.get_in_channel_value(5) < CH5POS1 - CH5THR and serial_con.get_in_channel_value(5) > CH5POS1 + CH5THR) {
-                    if(sock_pack_in.field0 == 1)
-                      ch0_off_goal = -20;
+                    // if(sock_pack_in.field0 == 1)
+                    //   ch0_off_goal = -20;
                 }
-
-                sock_pack_out.field0 = serial_con.get_out_channel_value(0);
-                sock_pack_out.field1 = serial_con.get_out_channel_value(1);
-                sock_pack_out.field2 = serial_con.get_out_channel_value(2);
-                sock_pack_out.field3 = serial_con.get_out_channel_value(3);
-                sock_pack_out.field4 = serial_con.get_out_channel_value(4);
-                sock_pack_out.field5 = serial_con.get_out_channel_value(5);
-                sock_pack_out.field6 = serial_con.get_out_channel_value(6);
-                sock_pack_out.field7 = 0;
-
+                //
+                // sock_pack_out.field0 = serial_con.get_out_channel_value(0);
+                // sock_pack_out.field1 = serial_con.get_out_channel_value(1);
+                // sock_pack_out.field2 = serial_con.get_out_channel_value(2);
+                // sock_pack_out.field3 = serial_con.get_out_channel_value(3);
+                // sock_pack_out.field4 = serial_con.get_out_channel_value(4);
+                // sock_pack_out.field5 = serial_con.get_out_channel_value(5);
+                // sock_pack_out.field6 = serial_con.get_out_channel_value(6);
+                // sock_pack_out.field7 = 0;
+                //
                 break;
         }
         serial_con.DSM_analyse(false); /* RX AND TX */
