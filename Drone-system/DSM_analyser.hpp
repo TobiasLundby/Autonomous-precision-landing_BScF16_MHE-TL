284f32fbf8ff2e67d06707286d4dcb12b741c88d
Tobias Lundby <tobiaslundby@gmail.com>
2016-05-23 23:15:39 +0200

    Added time test to DSM_analyser

--- Drone-system/DSM_analyser.hpp
+++ Drone-system/DSM_analyser.hpp
@@ -394,9 +394,9 @@ void DSM_RX_TX::RX_TX()
                                 sync_value = (256*old_byte_in)+byte_in;
                                 //printf("******* Preamble ********* Sync_val: %i\n",sync_value);
 
-                                if((sync_value == sync_value_expected or (sync_value >= sync_value_expected and sync_value <= sync_value_expected + MAX_ERROR_BETWEEN_PACKETS))
+                                if(((sync_value == sync_value_expected or (sync_value >= sync_value_expected and sync_value <= sync_value_expected + MAX_ERROR_BETWEEN_PACKETS)) and ((time_byte - time_last_byte) > frame_timeout))
                                     || (((sync_value_expected_next - SYNC_TOLERANCE) < sync_value)
-                                    && (sync_value < (sync_value_expected_next + SYNC_TOLERANCE))))
+                                    && (sync_value < (sync_value_expected_next + SYNC_TOLERANCE)) and ((time_byte - time_last_byte) > frame_timeout)))
                                 {
                                     if (sync_value > sync_value_expected and sync_value <= sync_value_expected + MAX_ERROR_BETWEEN_PACKETS)
                                     {
@@ -477,7 +477,7 @@ void DSM_RX_TX::RX_TX()
                 serialPutchar(ser_handle,byte_in); //TX byte
 
                 sync_value = (256*old_byte_in)+byte_in;
-                if((sync_value == sync_value_expected or (sync_value >= sync_value_expected and sync_value <= sync_value_expected + MAX_ERROR_BETWEEN_PACKETS)) ||
+                if(((sync_value == sync_value_expected or (sync_value >= sync_value_expected and sync_value <= sync_value_expected + MAX_ERROR_BETWEEN_PACKETS)) and ((time_byte - time_last_byte) > frame_timeout)) ||
                     (((sync_value_expected_next - SYNC_TOLERANCE) < sync_value) &&
                        (sync_value < (sync_value_expected_next + SYNC_TOLERANCE))) &&
                     ((time_byte - time_last_byte) > frame_timeout))
